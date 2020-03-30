34ba1e73925ac40c3cfa42ce2f4d4bd07bbaa039
Micke <michael.hjortholt@gmail.com>
2020-03-30 22:11:08 +0200

    always use https ?

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-/*
+
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
-}*/
+}
 
 const app = express();
 
-//app.use(requireHTTPS);
+app.use(requireHTTPS);
 app.use(express.json());
 
 app.post('/api/create', (req, res) => {
